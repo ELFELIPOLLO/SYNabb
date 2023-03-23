dbbc1e35700e26eecea39b7cea70673cc22fe2b4
ELFELIPOLLO <luis.ibarra@cetis107.edu.mx>
2023-03-23 08:31:26 -0700

    1.0

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -26,27 +26,27 @@ class ReportesController extends Controller
     }
 
     function consultar2AMCO(){
-        $reportes = reporte::where('grupotabla', 'x2amcoalumnos')->paginate(12);
+        $reportes = reporte::where('grupotabla', 'x2amcoalumnos')->paginate(7);
         $grupo = '2AMCO';
         return view('reportes.tablaReportes', compact('reportes','grupo'));
     }
     function consultar6AMP(){
-        $reportes = reporte::where('grupotabla', 'x6ampalumnos')->paginate(12);
+        $reportes = reporte::where('grupotabla', 'x6ampalumnos')->paginate(7);
         $grupo = '6AMP';
         return view('reportes.tablaReportes', compact('reportes','grupo'));
     }
     function consultar4AMC(){
-        $reportes = reporte::where('grupotabla', 'x4amcalumnos')->paginate(12);
+        $reportes = reporte::where('grupotabla', 'x4amcalumnos')->paginate(7);
         $grupo = '4AMC';
         return view('reportes.tablaReportes', compact('reportes','grupo'));
     }
     function consultar6AMO(){
-        $reportes = reporte::where('grupotabla', 'x6amoalumnos')->paginate(12);
+        $reportes = reporte::where('grupotabla', 'x6amoalumnos')->paginate(7);
         $grupo = '6AMO';
         return view('reportes.tablaReportes', compact('reportes','grupo'));
     }
     function consultar2AME(){
-        $reportes = reporte::where('grupotabla', 'x2amealumnos')->paginate(12);
+        $reportes = reporte::where('grupotabla', 'x2amealumnos')->paginate(7);
         $grupo = '2AME';
         return view('reportes.tablaReportes', compact('reportes','grupo'));
     }
